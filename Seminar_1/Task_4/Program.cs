753fa061adc4d2e8b4f3228177d75c9bc3c52677
Rushan <ektoeror777@gmail.com>
2022-12-06 19:57:43 +0300

    Task4

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+string n = Console.ReadLine()?? "";
+
+int n1 = Convert.ToInt32(n);
+
+n1 = Math.Abs(n1);
+
+for (int i = 2; i <= n1; i++)
+{
+    if (i % 2 ==0)
+    
+        Console.Write(i+" ");
+    
+}
