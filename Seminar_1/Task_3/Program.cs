3797e7d68387f4e8f4b487e3beaa57906ba5e230
Rushan <ektoeror777@gmail.com>
2022-12-06 19:42:34 +0300

    Task3

---
+++ Seminar_1/Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+string n = Console.ReadLine()?? "";
+
+int n1 = Convert.ToInt32(n);
+
+if (n1 % 2 >0)
+    Console.WriteLine($"Число {n1} нечетное");
+else
+    Console.WriteLine($"Число {n1} четное");
