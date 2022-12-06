07a3cc4d447b7a87fbc84398f429921a3b0a912a
Rushan <ektoeror777@gmail.com>
2022-12-06 19:20:13 +0300

    Task1

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа");
+string number1 = Console.ReadLine() ?? "";
+string number2 = Console.ReadLine() ?? "";
+
+int n1 = Convert.ToInt32(number1);
+int n2 = Convert.ToInt32(number2);
+
+if (n1>n2)
+    Console.WriteLine($"Число {n1} больше {n2}");
+else 
+    Console.WriteLine($"Число {n2} больше {n1}");
