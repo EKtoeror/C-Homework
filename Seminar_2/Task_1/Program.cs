d2faad4d0064a5f225a294a10f9af76d67116123
Rushan <ektoeror777@gmail.com>
2022-12-09 18:31:21 +0300

    задача 2.1

---
+++ Seminar_2/Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите трехзначное число : ");
+string n = Console.ReadLine() ?? "";
+
+int number = Convert.ToInt32(n);
+
+number = Math.Abs(number);
+
+void ShowSecondDigit(int number)
+{
+    if ((number >= 99) && (number <= 1000))
+    {
+        int number0 = (number / 10) % 10;
+        Console.WriteLine($"Вторая цыфра числа {number} - {number0}");
+    }
+    else
+    {
+        Console.WriteLine("Ввели неверное число");
+    }
+}
+
+
+ShowSecondDigit(number);
