3e5fa4f36ec28b02bb5aa4ddd581cef738191219
Rushan <ektoeror777@gmail.com>
2022-12-09 18:54:50 +0300

    задача 2.2

---
+++ Seminar_2/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+string n = Console.ReadLine() ?? "";
+
+int number = Convert.ToInt32(n);
+
+number = Math.Abs(number);
+
+void ShowThirdDigit(int number)
+{
+    if (number > 99)
+    {
+        //int number0 = number % 10;
+        //Console.WriteLine($"Третья цыфра числа {number} - {number0}");
+        string number0 = Convert.ToString(number);
+        Console.WriteLine($"Третья цыфра числа {number} - {number0[2]}");
+    }
+    else
+    {
+        Console.WriteLine("Третьей цыфры нет");
+    }
+}
+
+ShowThirdDigit(number);
---
+++ Seminar_2/Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число");
+string day = Console.ReadLine()?? "";
+
+switch (day) //
+{
+    case "1":
+        Console.WriteLine("Пн");
+        break;
+    case "2":
+        Console.WriteLine("Вт");
+        break;
+    case "3":
+        Console.WriteLine("Ср");
+        break;
+    case "4":
+        Console.WriteLine("Чт");
+        break;
+    case "5":
+        Console.WriteLine("Пт");
+        break;
+    case "6":
+        Console.WriteLine("Сб");
+        break;
+    case "7":
+        Console.WriteLine("Вс");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня недели");
+        break;
+}
