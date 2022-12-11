e95086ee76b3c46ae5fd25c90362b1ab84c7b8c1
Rushan <ektoeror777@gmail.com>
2022-12-12 00:38:08 +0300

    3.2

---
+++ Seminar_3/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты двух точек:  ");
+
+Console.WriteLine("Введите X1,Y1,Z1:  ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите X2,Y2,Z1:  ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+
+Console.WriteLine($"Растояние между точками {result}");
