3ef53ac94c3b194db0a08a4802db51b6d07be450
Rushan <ektoeror777@gmail.com>
2022-12-12 00:41:21 +0300

    3.3

---
+++ Seminar_3/Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i =1 ; i<= number ; i++)
+{
+    int result = i*i*i;
+    Console.WriteLine($" {i}^3 = {result}");
+}
