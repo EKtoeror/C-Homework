eb3d80283933cbe2b8f2c6f2758acbb2fea8b0a7
Rushan <ektoeror777@gmail.com>
2022-12-12 00:10:32 +0300

    3.1

---
+++ Seminar_3/Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите 5ти значное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+
+static extern void CheckPoli(int number);
+
+        
