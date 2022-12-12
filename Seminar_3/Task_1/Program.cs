c131605e5b6dc2ae7e4fc6c38edd96be09122875
Rushan <ektoeror777@gmail.com>
2022-12-12 19:37:38 +0300

    3.1_edit

--- Seminar_3/Task_1/Program.cs
+++ Seminar_3/Task_1/Program.cs
@@ -2,8 +2,32 @@
 
 int number = Convert.ToInt32(Console.ReadLine());
 
-number = Math.Abs(number);
+if (number>9999&&number<99999)
+{
+    void Check(int number)
+    {
+        string n=Convert.ToString(number);
+        int dig1= Convert.ToInt32(n[0]);
+        int dig2= Convert.ToInt32(n[1]);
+        int dig4= Convert.ToInt32(n[3]);
+        int dig5= Convert.ToInt32(n[4]);
+        if (dig1==dig5&&dig2==dig4)
+        {
+            Console.WriteLine($"Число {number} является полиморфом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не является полиморфом");
+        }
+    }
+    Check(number);
+}
 
-static extern void CheckPoli(int number);
+else
+{
+    {
+        Console.WriteLine("Введено неверное число");
+    }
+}   
 
         
