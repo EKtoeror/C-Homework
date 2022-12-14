c70743e3b5f9a90960c0cdbdb7447ed34e70ec7c
Rushan <ektoeror777@gmail.com>
2022-12-14 22:18:26 +0300

    3.1

---
+++ Seminar_4/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿void FillArray(int[] collection)
+{
+    int length = 8;//collection.Length;
+    
+    for (int i=0;i < length; i++)
+    {
+        collection[i] = new Random().Next(1, 10);
+        Console.Write(collection[i]);
+        if (i < length - 1) 
+            {
+                Console.Write(",");
+            }
+    }
+}
+
+int[] array = new int[8]; 
+FillArray(array);
+
+
+
