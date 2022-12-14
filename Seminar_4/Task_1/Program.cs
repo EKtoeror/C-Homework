6574f65e10a661f5bafe1bb95171b51920c53269
Rushan <ektoeror777@gmail.com>
2022-12-14 23:04:48 +0300

    4.1

---
+++ Seminar_4/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+b = Math.Abs(b);
+
+int exp=a;
+for (int i = 1; i < b; i++)
+{
+    exp = a * exp;
+}
+Console.Write(exp);
